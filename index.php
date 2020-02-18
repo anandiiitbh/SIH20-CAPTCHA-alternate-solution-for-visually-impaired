f3110d7310aceb50720cc8bad5536a22efa84aa9
anandiiitbh <61147494+anandiiitbh@users.noreply.github.com>
2020-02-18 17:37:19 +0530

    initial Codes after campus hackathon

---
+++ index.php
@@ -0,0 +1,97 @@
+
+
+    <?php
+	session_start();
+	?>
+<html>
+
+<head>
+
+<title>SIH
+
+</title>
+
+<link rel="icon" href="logo1.png" type="image/icon type">
+<link rel="stylesheet" type="text/css" href="login.css">
+
+</head>
+
+<body background="books.gif" style="background-repeat: no-repeat;
+  background-size: 1500px 1000px; opacity: 10 " >
+  
+
+
+<center>
+<h1>Indian Institute Of Information Technology, Bhagalpur</h1><br>
+
+<a href="index.php">
+
+<img src="iiitbh.png" alt="Indian Institute Of Information Technology, Bhagalpur" height="130" width="130">
+
+</img>
+<img src="logo.png" alt="Indian Institute Of Information Technology, Bhagalpur" height="110" width="130">
+
+</img>
+
+</img>
+<img src="drdoo.png" alt="Indian Institute Of Information Technology, Bhagalpur" height="110" width="130">
+
+</img>
+
+</a>
+
+<form action="login.php" method = "post">
+
+<div class="container">
+
+
+<input type="text" placeholder="Enter Username" name="username" required>
+ 
+</div>
+<div>
+<input type="password" placeholder="Enter Password" name="password" required>
+</div>
+<div>
+<style>/* Top left text */
+.click {
+  position: absolute;
+  top: 370px;
+  left: 600px;
+}
+</style>
+<img src="oooo.jpg" alt="Workplace" usemap="#workmap" width="200" height="179">
+<h4>Click on "Hello" to Submit</h4>
+<map name="workmap">
+  <area shape="rect" coords="5,40,35,50" alt="Computer" href="rrr.php">
+</map>
+
+
+<audio controls   accesskey="h">
+  <?php	
+		$capt = array("30", "10", "8" , "13" , "11" );
+		$num=$capt[rand(0,4)];
+		$_SESSION['capp']=$num;
+  
+  echo'	<source src="'.$num.'.mp3" type="audio/mpeg" >
+Your browser does not support the audio element.
+</audio></div>
+<input type="text" placeholder = "Enter Captcha" name="fname"><br>
+<div>
+   
+<input name="submit" type="submit" value=" Login "><br>
+<a href="/pass/forgot_password.php">forgot passowrd</a><br>
+<a href="/pass/signup.php">New User?? Sign Up</a>
+
+</div>
+
+
+
+
+</form>
+
+</center>
+
+</body>
+
+</html>';
+?>
---
+++ login.php
@@ -0,0 +1,24 @@
+    <?php
+	
+    session_start();// Starting Session
+    $error = ''; // Variable To Store Error Message
+	$_SESSION['val']=0;
+    if (isset($_POST['submit'])) {
+		include('AND/php/session.php');
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+    $error = "Username or Password is invalid";
+    }
+    else{$_SESSION['val']=1;
+    // Define $username and $password
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+	$captcha = $_POST['fname'];
+	if(!strcmp($captcha,$_SESSION['capp'])){
+		echo "<h1>Validation Success</h1>";
+	}
+	else{
+		echo "<h1>invalid Captcha</h1>";
+	}
+	}
+	}
+    ?>
